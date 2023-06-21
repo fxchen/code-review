77b293e2fb27d9259078cda141343da127658857
Frank Chen <frxchen@gmail.com>
2023-06-21 16:40:15 -0700

    Update arguments for CLI usage (#7)
    * Update input
    
    * Update model as an arg
    
    * Update inputs
    
    * Use checkout@v2
    
    * Update action.yml

--- code_review.py
+++ code_review.py
@@ -3,21 +3,9 @@
 import argparse
 import os
 import git
-import openai
+import subprocess
 
 REQUEST = "Reply on how to improve the code for style, clarity, comments, and tests (below)\n"
-
-STYLES = {
-  "zen": "Format feedback in the style of a zen koan",
-  "concise": "Format feedback concisely with numbered list"
-}
-
-PERSONAS = {
-  "developer": "You are an experienced software developer in a variety of programming languages and methodologies. You create efficient, scalable, and fault-tolerant solutions",
-  "kent_beck": "You are Kent Beck. You are known for software design patterns, test-driven development (TDD), and agile methodologies",
-  "marc_benioff": "You are Marc Benioff, internet entrepreneur and experienced software developer",
-  "yoda": "You are Yoda, legendary Jedi Master. Speak like Yoda",
-}
 
 def get_file(filename):
   """Get the contents of the specified file."""
@@ -36,24 +24,24 @@ def get_diff(filename=None):
     try:
       repo = git.Repo(search_parent_directories=True)
       current_branch = repo.active_branch.name
-      diff = repo.git.diff('origin/master..' + current_branch)
-      
+      diff = repo.git.diff('origin/main..' + current_branch)
+
       if diff:
         return diff
       else:
         return "No differences between master and current branch."
-    
+
     except git.GitError as e:
       return f"An error occurred while trying to get the git diff: {str(e)}"
 
 def main():
   # Get arguments
-  parser = argparse.ArgumentParser(description="Get the git diff from master of the current branch.")
+  parser = argparse.ArgumentParser(description="Improve your pull requests and code base with AI-assisted code reviews")
   parser.add_argument('--persona', default='developer', help='The persona to use in the prompt (developer, kent_beck, marc_benioff, yoda)')
   parser.add_argument('--style', default='concise', help='The style of output to use (concise, zen)')
   parser.add_argument('--filename', default=None, help='Optional filename to use instead of git diff')
   parser.add_argument('--dir', type=str, default=None, help='Optional directory to use instead of git diff')
-
+  parser.add_argument('--model', default='gpt-3.5-turbo', help='The model to use for the OpenAI API call')
 
   args = parser.parse_args()
 
@@ -65,31 +53,19 @@ def main():
         for file in files:
           file_path = os.path.join(root, file)
           diff += get_diff(file_path)
-          # Further processing on diff
     else:
       print(f"The provided directory {args.dir} does not exist.")
   else:
     diff = get_diff(args.filename)
 
-  # Set up prompt
-  BACKGROUND = PERSONAS[args.persona]
-  STYLE = STYLES[args.style]
-  prompt = f"{BACKGROUND}.{STYLE}.{REQUEST}\n{diff}"
-  print(f"PROMPT:\n{prompt}")
+  # Set environment variables
+  os.environ["MODEL"] = args.model
+  os.environ["PERSONA"] = args.persona
+  os.environ["STYLE"] = args.style
 
-  # Set up request to OpenAI
-  openai.api_key = os.getenv("OPENAI_API_KEY")
-  response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-          {"role": "system", "content": prompt},
-      ]
-  )
-
-  # Output response
-  print(f"RESPONSE:\n{response}")
-  completion_text = response.choices[0].message.content.strip()
-  print(f"FORMATTED RESPONSE:\n{completion_text}")
+  # Call action_code_review.py with the diff as input
+  process = subprocess.run(["python3", "action_code_review.py"], input=diff, text=True, capture_output=True)
+  print(process.stdout)
 
 if __name__ == "__main__":
   main()
