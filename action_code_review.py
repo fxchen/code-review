922a0ada68bbc3b027912c4596bd32be983febe5
Frank Chen <frxchen@gmail.com>
2023-06-27 12:47:45 -0700

    Include format_file_contents function

--- action_code_review.py
+++ action_code_review.py
@@ -2,6 +2,41 @@
 import os
 import sys
 import openai
+import re
+
+def extract_filenames_from_diff(diff_text):
+    """
+    This function extracts filenames from git diff text using regular expressions.
+    
+    :param diff_text: str, git diff text
+    :return: list of str, list of filenames
+    """
+
+    # Pattern for lines that start with '+++ b/', which are lines that contain filenames
+    # of the "new version" files in the diff. The parentheses (.) create a group that
+    # captures the filename that follows '+++ b/'.
+    pattern = r'\+\+\+ b/(.*)'
+
+    # re.findall function finds all matches of the pattern in the diff_text
+    # and returns them as a list.
+    filenames = re.findall(pattern, diff_text)
+
+    return filenames
+
+def format_file_contents(filenames):
+    """
+    This function iteratively goes through each filename and concatenates
+    the filename and its content in a specific markdown format.
+
+    :param filenames: list of str, list of filenames
+    :return: str, formatted string
+    """
+    files_str = ""
+    for filename in filenames:
+        with open(filename, 'r') as file:
+            content = file.read()
+        files_str += f"\n{filename}\n```\n{content}\n```\n"
+    return files_str
 
 REQUEST = "Reply on how to improve the code for style, clarity, comments, and tests (below)\n"
 
@@ -21,6 +56,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 model = os.environ["MODEL"]
 persona = PERSONAS[os.environ["PERSONA"]]
 style = STYLES[os.environ["STYLE"]]
+use_full_file = os.environ["
 
 # Read in the diff
 diff = sys.stdin.read()
